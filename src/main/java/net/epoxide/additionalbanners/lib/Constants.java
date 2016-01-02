33cc0d6d5045cbbe0be43461074d4337f4bda115
Darkhax <darklime@live.ca>
2016-01-02 01:28:55 -0700

    Updated to work on 1.8->1.8.9

--- src/main/java/net/epoxide/additionalbanners/lib/Constants.java
+++ src/main/java/net/epoxide/additionalbanners/lib/Constants.java
@@ -10,6 +10,7 @@ public class Constants {
     public static final String MOD_ID = "additionalbanners";
     public static final String MOD_NAME = "Additional Banners";
     public static final String VERSION_NUMBER = "1.0.0";
+    public static final String MCVERSION = "[1.8,1.8.9]";
     public static final String CLIENT_PROXY_CLASS = "net.epoxide.additionalbanners.client.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "net.epoxide.additionalbanners.common.CommonProxy";
     public static final Random RANDOM = new Random();
