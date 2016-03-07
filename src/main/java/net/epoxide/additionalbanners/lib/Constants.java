7a1d4e586744ea268c24fc15661e4899405f8c8c
Darkhax <darklime@live.ca>
2016-03-06 23:08:41 -0700

    Allow the mod to run on 1.9, even though forge hasn't released yet.

--- src/main/java/net/epoxide/additionalbanners/lib/Constants.java
+++ src/main/java/net/epoxide/additionalbanners/lib/Constants.java
@@ -2,17 +2,13 @@ package net.epoxide.additionalbanners.lib;
 
 import java.util.Random;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public class Constants {
     
     public static final String MOD_ID = "additionalbanners";
     public static final String MOD_NAME = "Additional Banners";
     public static final String VERSION_NUMBER = "1.1.1";
-    public static final String MCVERSION = "[1.8,1.8.9]";
+    public static final String MCVERSION = "[1.8,1.9.99.99]";
     public static final String CLIENT_PROXY_CLASS = "net.epoxide.additionalbanners.client.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "net.epoxide.additionalbanners.common.CommonProxy";
     public static final Random RANDOM = new Random();
-    public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 }
