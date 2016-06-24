bfbfef8234165f2c29a11dd40dba2a752687357b
GirafiStudios <girafi0@gmail.com>
2016-06-24 22:41:49 +0200

    Ported to 1.10

--- src/main/java/net/darkhax/additionalbanners/lib/Constants.java
+++ src/main/java/net/darkhax/additionalbanners/lib/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
     
     public static final String MOD_ID = "additionalbanners";
     public static final String MOD_NAME = "Additional Banners";
-    public static final String VERSION_NUMBER = "1.2.0.0";
+    public static final String VERSION_NUMBER = "1.3.0.0";
 }
