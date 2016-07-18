a50082a26b11748ce2ee4f00c00dc635db2296fb
Tyler Hancock <darklime@live.ca>
2016-07-18 00:52:33 -0600

    Updated for 1.10.2

--- src/main/java/net/darkhax/additionalbanners/AdditionalBanners.java
+++ src/main/java/net/darkhax/additionalbanners/AdditionalBanners.java
@@ -7,7 +7,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Constants.MOD_ID, name = Constants.MOD_NAME, version = Constants.VERSION_NUMBER)
+@Mod(modid = Constants.MOD_ID, name = Constants.MOD_NAME, version = Constants.VERSION_NUMBER, acceptedMinecraftVersions = "[1.9.4,1.10.2]")
 public class AdditionalBanners {
     
     @Mod.Instance(Constants.MOD_ID)
